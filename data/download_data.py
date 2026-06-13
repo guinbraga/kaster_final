2758f0b0daf71dc5da9bef1f511158c83b424e1c
guinbraga <guilhormo.47@gmail.com>
2026-06-13 00:01:50 -0300

    fix: download all data

--- data/download_data.py
+++ data/download_data.py
@@ -12,15 +12,25 @@ for year in years:
     url = f"https://balanca.economia.gov.br/balanca/bd/comexstat-bd/ncm/EXP_{year}.csv"
     current_file = f"EXP_{year}.csv"
     if current_file not in dir_files:
-        subprocess.run(["wget", url])
+        subprocess.run(["wget", "--no-check-certificate", url])
+dir_files = os.listdir()
 
 # Make file with concatenation of all files:
 if "all_exports.csv" not in dir_files:
     export_files = [file for file in dir_files if file[:3] == "EXP"]
     with open("all_exports.csv", "w", encoding="latin-1") as all_exports:
-        for file in export_files:
+        for i, file in enumerate(export_files):
             print(f"concatenating file {file}...")
             with open(file, "r", encoding="latin-1") as csv_file:
-                for line in csv_file.readlines():
+                if i > 0:
+                    csv_file.readline() # so we do not write the header twice
+                else:
+                    all_exports.write(csv_file.readline().lower())
+
+                for line in csv_file:
                     all_exports.write(line)
     print("Created file all_exports.csv containing all exports data!")
+
+# Download NCM files:
+url = "https://balanca.economia.gov.br/balanca/bd/tabelas/NCM.csv"
+subprocess.run(["wget", "--no-check-certificate", url])
